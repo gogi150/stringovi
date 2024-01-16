e57bf77e862cd3c52bfbf69a4cf96784bb9b1cbb
gogi150 <153612601+gogi150@users.noreply.github.com>
2024-01-16 12:42:00 +0100

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kokolo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite niz znakova:");
+            string unos = Console.ReadLine();
+
+            string zamijenjeniZnakovi = unos.Replace(" ", "_");
+
+            Console.WriteLine("Niz znakova s zamijenjenim razmacima:");
+            Console.WriteLine(zamijenjeniZnakovi);
+            Console.ReadLine();
+        }
+    }
+}
